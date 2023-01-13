f65a66a0eb6fd09b87a2f5d1ae4dbcb45b025884
Стамо Петков <s.g.petkov@is-bg.net>
2023-01-14 00:21:20 +0200

    Service collection extension added

---
+++ TelloSDK/Extensions/TelloSDKPilotServiceCollectionExtension.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using TelloSDK.Contracts;
+using TelloSDK.Infrastructure.Models;
+using TelloSDK.Pilot.Contracts;
+using TelloSDK.Pilot.Services;
+using TelloSDK.Services;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Add Tello SDK Pilot to IoC
+    /// </summary>
+    public static class TelloSDKPilotServiceCollectionExtension
+    {
+        /// <summary>
+        /// Adding Tello SDK Pilot with default configuration
+        /// </summary>
+        /// <param name="services">Service collection</param>
+        /// <returns></returns>
+        public static IServiceCollection AddTelloSDKPilot(this IServiceCollection services)
+        {
+            Action<TelloOptions> options = option => new TelloOptions()
+            {
+                IPAddress = IPAddress.Parse("192.168.10.1"),
+                Port = 8889
+            };
+
+            AddTelloSDKPilot(services, options);
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adding Tello SDK Pilot with customizable configuration options
+        /// </summary>
+        /// <param name="services">Service collection</param>
+        /// <param name="options">Configuration options</param>
+        /// <returns></returns>
+        public static IServiceCollection AddTelloSDKPilot(this IServiceCollection services, Action<TelloOptions> options)
+        {
+            services.Configure(options);
+            services.AddSingleton<ITelloCommandClient, TelloCommandClient>();
+            services.AddScoped<ITelloValidationService, TelloValidationService>();
+            services.AddScoped<IPilot, Pilot>();
+            services.AddScoped<IREPLService, REPLService>();
+            services.AddScoped<IFlightPlan, FlightPlan>();
+
+            return services;
+        }
+    }
+}
