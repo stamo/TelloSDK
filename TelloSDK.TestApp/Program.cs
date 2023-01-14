f474d5f4d75656d08c5fbf5f0f4f1a1056db3f44
Стамо Петков <s.g.petkov@is-bg.net>
2023-01-14 22:03:01 +0200

    Console app for testing added

---
+++ TelloSDK.TestApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TelloSDK.Pilot.Contracts;
+
+// Building IoC container
+IServiceProvider provider = new ServiceCollection()
+    .AddTelloSDKPilot()
+    .BuildServiceProvider();
+
+// Get FlightPlan service
+var flightPlan = provider.GetService<IFlightPlan>();
+
+if (flightPlan != null)
+{
+    flightPlan
+        .TakeOff()
+        .Backward(50)
+        .Up(50)
+        .Flip(TelloSDK.Enumerations.Direction.Back)
+        .Forward(50)
+        .Right(50)
+        .Land()
+        .Validate()
+        .Execute();
+}
