c6f2e0d93108049321e2e3e718add44e3439f47c
Стамо Петков <s.g.petkov@is-bg.net>
2023-01-15 23:49:19 +0200

    Some test methods added

--- TelloSDK.TestApp/Program.cs
+++ TelloSDK.TestApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using TelloSDK.Contracts;
 using TelloSDK.Pilot.Contracts;
 
 // Building IoC container
@@ -6,19 +7,67 @@ IServiceProvider provider = new ServiceCollection()
     .AddTelloSDKPilot()
     .BuildServiceProvider();
 
-// Get FlightPlan service
+// Get Tello SDK services
 var flightPlan = provider.GetService<IFlightPlan>();
+var repl = provider.GetService<IREPLService>();
+var telloPilot = provider.GetService<IPilot>();
 
 if (flightPlan != null)
 {
+    TestFlightPlan(flightPlan);
+}
+
+//if (repl != null)
+//{
+//    TestRepl(repl);
+//}
+
+//if (telloPilot != null)
+//{
+//    TestTelloPilot(telloPilot);
+//}
+
+void TestFlightPlan(IFlightPlan flightPlan)
+{
     flightPlan
-        .TakeOff()
-        .Backward(50)
-        .Up(50)
-        .Flip(TelloSDK.Enumerations.Direction.Back)
-        .Forward(50)
-        .Right(50)
-        .Land()
-        .Validate()
-        .Execute();
+            .TakeOff()
+            .Backward(50)
+            .Up(50)
+            .Flip(TelloSDK.Enumerations.Direction.Back)
+            .Forward(50)
+            .Right(50)
+            .Land()
+            .Validate()
+            .Execute();
+}
+
+void TestRepl(IREPLService repl)
+{
+    string command = Console.ReadLine() ?? "quit";
+
+    while (command != "quit")
+    {
+        Console.WriteLine(repl.ExecuteCommand(command));
+
+        command = Console.ReadLine() ?? "quit";
+    }
+}
+
+void TestTelloPilot(IPilot telloPilot)
+{
+    var result = telloPilot.Ignition();
+    Console.WriteLine(result.Message);
+    result = telloPilot.TakeOff();
+    Console.WriteLine(result.Message);
+    result = telloPilot.Backward(50);
+    Console.WriteLine(result.Message);
+    result = telloPilot.Up(50);
+    Console.WriteLine(result.Message);
+    result = telloPilot.Flip(TelloSDK.Enumerations.Direction.Right);
+    Console.WriteLine(result.Message);
+    result = telloPilot.Forward(150);
+    Console.WriteLine(result.Message);
+    result = telloPilot.Land();
+    Console.WriteLine(result.Message);
+    telloPilot.EnginesOff();
 }
