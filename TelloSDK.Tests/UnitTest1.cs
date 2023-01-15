d7fabfe34f812b8a3d4f7f9dc5d7a58be5851294
Стамо Петков <s.g.petkov@is-bg.net>
2023-01-16 00:11:15 +0200

    Unit tests project added

---
+++ TelloSDK.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TelloSDK.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
