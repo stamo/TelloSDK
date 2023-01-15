82e20fe93a0e5a4e59cf993b07f5d66e473ae962
Стамо Петков <s.g.petkov@is-bg.net>
2023-01-16 00:02:22 +0200

    Some commenst added

--- TelloSDK.TestApp/Program.cs
+++ TelloSDK.TestApp/Program.cs
@@ -27,6 +27,8 @@ if (flightPlan != null)
 //    TestTelloPilot(telloPilot);
 //}
 
+// The drone will fly according to
+// predefind flight plan
 void TestFlightPlan(IFlightPlan flightPlan)
 {
     flightPlan
@@ -41,6 +43,8 @@ void TestFlightPlan(IFlightPlan flightPlan)
             .Execute();
 }
 
+// User can input commands in real time
+// to controll the drone
 void TestRepl(IREPLService repl)
 {
     string command = Console.ReadLine() ?? "quit";
@@ -53,21 +57,39 @@ void TestRepl(IREPLService repl)
     }
 }
 
+// Testing IPilot SDK Interface
+// You can run commands according to
+// user iteractions or program flow
 void TestTelloPilot(IPilot telloPilot)
 {
+    // Initialize client
     var result = telloPilot.Ignition();
     Console.WriteLine(result.Message);
+
+    // Take the drone in air
     result = telloPilot.TakeOff();
     Console.WriteLine(result.Message);
+
+    // Fly back 50 cm
     result = telloPilot.Backward(50);
     Console.WriteLine(result.Message);
+
+    // Ascend 50 cm
     result = telloPilot.Up(50);
     Console.WriteLine(result.Message);
+
+    // Make a flir to the right
     result = telloPilot.Flip(TelloSDK.Enumerations.Direction.Right);
     Console.WriteLine(result.Message);
+
+    // Fly forward 150 cm
     result = telloPilot.Forward(150);
     Console.WriteLine(result.Message);
+
+    // Land
     result = telloPilot.Land();
     Console.WriteLine(result.Message);
+
+    // Destroy client
     telloPilot.EnginesOff();
 }
