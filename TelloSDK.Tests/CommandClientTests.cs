5a08ab5377d4ab8aac12383b5e53ae772eb71cca
Стамо Петков <s.g.petkov@is-bg.net>
2023-01-16 18:10:47 +0200

    Command client tests started

---
+++ TelloSDK.Tests/CommandClientTests.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using TelloSDK.Models;
+using TelloSDK.Pilot.Contracts;
+using TelloSDK.Pilot.Services;
+
+namespace TelloSDK.Tests
+{
+    public class CommandClientTests
+    {
+        private Mock<UdpClient> moqUdpClient = new Mock<UdpClient>();
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            moqUdpClient.Setup(c => c.Send(
+                It.IsAny<byte[]>(), 
+                It.IsAny<int>(),
+                It.IsAny<IPEndPoint>()))
+                .Returns((byte[] b, int i, IPEndPoint ep) => i);
+            moqUdpClient.Setup(c => c.Receive(ref It.Ref<IPEndPoint?>.IsAny))
+                .Returns(Encoding.ASCII.GetBytes("ok"));
+        }
+
+        //[Test]
+        //public void TestExecuteCommand()
+        //{
+        //    var commandClient = new TelloCommandClient()
+        //}
+    }
+}
